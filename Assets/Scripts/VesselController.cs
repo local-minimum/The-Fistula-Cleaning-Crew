f8ac3b52302fe3882ad71707872bff3d28c42459
Martin Zackrisson <martin.zackrisson@fripost.org>
2021-04-24 11:30:46 +0200

    maybe a rotational stabilizer

--- Assets/Scripts/VesselController.cs
+++ Assets/Scripts/VesselController.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class VesselController : MonoBehaviour
 {
+    [SerializeField] float stabilizingForce = 1f;
+    [SerializeField] float stabilizingSpeedScale = 10f;
+    [SerializeField] AnimationCurve stabilizingSpeedModulation;
+
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponentInChildren<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        StabilizeVessel();    
+    }
+
+    float rotation
+    {
+        get
+        {
+            float rotation = Vector3.Angle(transform.up, Vector3.up);            
+            if (rotation < -180f) {
+                return rotation + 180f;
+            } else if (rotation > 180f)
+            {
+                return rotation - 180f;
+            }
+            return rotation;
+        }
+    }
+
+    void StabilizeVessel()
+    {
+        float velocity = rb.angularVelocity;
+        float targetVelocity = stabilizingSpeedModulation.Evaluate(Mathf.Abs(rotation)) * Mathf.Sign(rotation) * stabilizingSpeedScale;
+        if ((targetVelocity < 0 && velocity > targetVelocity) || (targetVelocity > 0 && velocity < targetVelocity))
+        {
+            rb.AddTorque(stabilizingForce * Time.deltaTime);
+        }
+        Debug.Log(string.Format("{0} {1} {2}", velocity, rotation, targetVelocity));
     }
 }
