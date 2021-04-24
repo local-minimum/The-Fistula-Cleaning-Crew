8ef39eaac1ee200fa9a82c916f111467e483db18
Martin Zackrisson <martin.zackrisson@fripost.org>
2021-04-24 21:15:51 +0200

    Added infos to level prefab

---
+++ Assets/Scripts/InfoBoards.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoBoards : MonoBehaviour
+{
+    [SerializeField] List<GameObject> boards = new List<GameObject>();
+    int index = -1;
+
+    private void Start()
+    {
+        OnClickNext();
+    }
+
+    public void OnClickNext()
+    {
+        if (index >= 0)
+        {
+            boards[index].SetActive(false);
+        }
+        index += 1;
+        if (index >= boards.Count)
+        {
+            index = 0;
+        }
+        boards[index].SetActive(true);
+    }
+}
